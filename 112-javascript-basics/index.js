dd058088518c961c106735cacb409e5a613a9ad1
ptchblvck <ptchblvck@icloud.com>
2022-11-11 07:42:31 +0100

    javascript exercises 1-14

--- 112-javascript-basics/index.js
+++ 112-javascript-basics/index.js
@@ -64,9 +64,175 @@ function calculateVolume(height, length, width) {
   return height * length * width;
 }
 
-function calcCircleArea(radius) {
+function calcCircleArea(radius, emptyParam) {
   return Math.PI * radius * radius;
 }
 
-let newCircle = calcCircleArea(4);
-console.log("Die Kreisflaeche betraegt: " + newCircle);
+function calculator(a = null, b = null, operator) {
+  if (operator == "sum") {
+    let resultSum = sum(a, b);
+    return console.log("Das Ergebnis wurde addiert und ergibt: " + resultSum);
+  }
+  if (operator == "subtract") {
+    let resultSub = subtract(a, b);
+    return console.log(
+      "Das Ergebnis wurde subtrahiert und ergibt: " + resultSub
+    );
+  }
+  if (operator == "multiply") {
+    let resultMultiply = multiply(a, b);
+    return console.log(
+      "Das Ergebnis wurde multipliziert und ergibt: " + resultMultiply
+    );
+  }
+  if (operator == "divide") {
+    let resultDivide = divide(a, b);
+    return console.log(
+      "Das Ergebnis wurde dividiert und ergibt: " + resultDivide
+    );
+  }
+  if (operator == "circle") {
+    let newCircle = calcCircleArea(a, b);
+    return console.log("Die Kreisflaeche betraegt: " + newCircle);
+  }
+}
+
+function sum(x, y) {
+  return x + y;
+}
+
+function subtract(x, y) {
+  return x - y;
+}
+
+function multiply(x, y) {
+  return x * y;
+}
+
+function divide(x, y) {
+  return x / y;
+}
+
+function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function orderMyLogic(val) {
+  if (val < 5) {
+    return "Less than 5";
+  } else if (val < 10) {
+    return "Less than 10";
+  } else {
+    return "Greater than or equal to 10";
+  }
+}
+
+function testElseIf(val) {
+  if (val > 10) {
+    return "Greater than 10";
+  } else if (val < 5) {
+    return "Smaller than 5";
+  } else {
+    return "Between 5 and 10";
+  }
+}
+
+function findTheRightSize(num = 0) {
+  if (num < 5) {
+    return "Tiny";
+  } else if (num >= 5 && num < 10) {
+    return "Small";
+  } else if (num >= 10 && num < 15) {
+    return "Medium";
+  } else if (num >= 15 && num <= 20) {
+    return "Large";
+  } else if (num > 20) {
+    return "Huge";
+  }
+}
+
+function showAlert(statusCode) {
+  let message;
+
+  if (statusCode == 1) {
+    message = "Alpha";
+  } else if (statusCode == 2) {
+    message = "Beta";
+  } else if (statusCode == 3) {
+    message = "Gamma";
+  } else if (statusCode == 4) {
+    message = "Delta";
+  } else {
+    message = "Unbekannter Wert";
+  }
+  return message;
+}
+
+function oddOrEven(intNumber) {
+  if (intNumber % 2 == 0) {
+    return "Even Number";
+  } else {
+    return "Odd Number";
+  }
+}
+
+function boolToWord(bool) {
+  if (bool == true) {
+    return "Yes";
+  } else if (bool == false) {
+    return "No";
+  } else {
+    return "Not a boolean value";
+  }
+}
+
+function take_umbrella(weather, chance) {
+  if (weather == "cloudy" && chance <= 0.2) {
+    console.log("you don't have to take the umbrella with you");
+    return false;
+  } else if (weather == "cloudy" && chance > 0.2) {
+    console.log("take the umbrella with you");
+    return true;
+  } else if (weather == "sunny" && chance < 0.5) {
+    console.log("you don't have to take the umbrella with you");
+    return false;
+  } else if (weather == "sunny" && chance >= 0.5) {
+    console.log("take the umbrella with you");
+    return true;
+  } else if (weather == "rainy") {
+    console.log("take the umbrella with you");
+    return true;
+  }
+}
+
+function rps(p1, p2) {
+  if (typeof p1 == "string" && typeof p2 == "string") {
+    let player1 = p1;
+    let player2 = p2;
+
+    if (player1 == "rock" && player2 == "scissors") {
+      return "Player1 wins!";
+    } else if (player1 == "scissors" && player2 == "paper") {
+      return "Player1 wins!";
+    } else if (player1 == "paper" && player2 == "rock") {
+      return "Player1 wins!";
+    } else if (player1 == "rock" && player2 == "paper") {
+      return "Player2 wins!";
+    } else if (player1 == "scissors" && player2 == "rock") {
+      return "Player2 wins!";
+    } else if (player1 == "paper" && player2 == "scissors") {
+      return "Player1 wins!";
+    } else if (player1 == "scissors" && player2 == "scissors") {
+      return "It's a Draw!";
+    } else if (player1 == "rock" && player2 == "rock") {
+      return "It's a Draw!";
+    } else if (player1 == "paper" && player2 == "paper") {
+      return "It's a Draw!";
+    } else {
+      return "Weapon not found!";
+    }
+  }
+  return "not a string input";
+}
