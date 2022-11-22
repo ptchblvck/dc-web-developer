e770c29c01ee8be1303eaa6458a7c613ea2adb81
ptchblvck <ptchblvck@icloud.com>
2022-11-22 09:28:01 +0100

    changed something

---
+++ SQL Schulung/Wiederholung.js
@@ -0,0 +1,40 @@
+/* Was ist eine Datenbank
+
+Abfragen mit SELECT * FROM Datenbanktabelle
+
+Rechnungen mit +, -, *, /
+
+PI() ist vorhanden
+
+AND & OR Operatoren
+
+String funktionen:
+
+replace, repeat, reverse, Substring(name, startzeichen (index = 1), endzeichen)
+
+length = anzahl zeichen
+
+upper & lower fuer zeichen uppercase/ lowercase
+
+
+
+SELECT * FROM Tabelle WHERE (where = diese kriterien muessen zutreffen -> kriterienabfrage)
+
+AS zur alias vergabe zB. SELECT Name AS Hauptstadt FROM country
+
+GROUP BY -> Gruppieren von Eintraegen mit kriterium
+
+ORDER BY -> Sortierung
+
+LIMIT -> Anzeige bis zum eingegebenen Wert
+
+
+FRAGEN:
+
+Wer hat die Datenbanken-SQL Sprache entwickelt?  ---- Antwort: IBM
+
+Was war die erste Datenbank?
+
+Was ist eine Subabfrage? Wie sieht eine Subabfrage aus?
+
+
