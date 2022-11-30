24fe32d4a97a5a85e14990e192c2cce10d992c68
ptchblvck <ptchblvck@icloud.com>
2022-11-30 11:20:49 +0100

    mysql to mariadb

--- 999-SQL-Schulung/mysql.js
+++ 999-SQL-Schulung/mysql.js
@@ -1,12 +1,25 @@
-var mysql = require("mysql");
-
-var con = mysql.createConnection({
+const mariadb = require("mariadb");
+const pool = mariadb.createPool({
   host: "localhost",
-  user: "joey",
-  password: "password",
+  port: "3306",
+  user: "ptchblvck",
+  password: "7932",
+  connectionLimit: 5,
 });
-
-con.connect(function (err) {
-  if (err) throw err;
-  console.log("Connected!");
-});
+async function asyncFunction() {
+  let conn;
+  try {
+    conn = await pool.getConnection();
+    const rows = await conn.query("SELECT 1 as val");
+    console.log(rows); //[ {val: 1}, meta: ... ]
+    const res = await conn.query("INSERT INTO myTable value (?, ?)", [
+      1,
+      "mariadb",
+    ]);
+    console.log(res); // { affectedRows: 1, insertId: 1, warningStatus: 0 }
+  } catch (err) {
+    throw err;
+  } finally {
+    if (conn) return conn.end();
+  }
+}
