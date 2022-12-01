dff4de6bf6ffaef6ddf972cc86dd843451d0a0fd
ptchblvck <ptchblvck@icloud.com>
2022-12-01 08:53:02 +0100

    added node modules

--- 999-SQL-Schulung/app.js
+++ 999-SQL-Schulung/app.js
@@ -1,14 +1,10 @@
 const express = require("express");
 const mysql = require("mysql2");
 var cors = require("cors");
-const mariadb = require("mariadb");
 
-const connection = mariadb.createConnection({
-  host: "localhost",
-  user: "ptchblvck",
-  password: "7932",
-  database: "mondial",
-});
+require("dotenv").config({ path: ".env" });
+// require("dotenv").config();
+// console.log(process.env); // remove this after you've confirmed it is working
 
 const app = express();
 const port = 3000;
@@ -16,12 +12,12 @@ const port = 3000;
 app.use(cors());
 
 // create the connection to database
-// const connection = mysql.createConnection({
-//   host: "localhost",
-//   user: "gyula",
-//   password: "gyula",
-//   database: "mondial",
-// });
+const connection = mysql.createConnection({
+  host: process.env.MYSQL_HOST,
+  user: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PWD,
+  database: process.env.MYSQL_DB,
+});
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
@@ -32,7 +28,7 @@ app.get("/unconditional", (req, res) => {
     'SELECT * FROM `city` WHERE `Country` = "A"',
     function (err, results, sql_fields) {
       console.log(results); // results contains rows returned by server
-      //console.log(fields); // fields contains extra meta data about results, if available
+      // console.log(fields); // fields contains extra meta data about results, if available
       let fields = [];
       sql_fields.forEach((element) => {
         fields.push({ name: element.name });
